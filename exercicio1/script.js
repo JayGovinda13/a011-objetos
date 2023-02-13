829527d58d675b2d4a5f110c0988a0ee4aea0930
Juliano Amaral <jaygovinda13@gmail.com>
2023-02-13 17:49:39 -0300

    revisão objetos feita

---
+++ exercicio1/script.js
@@ -0,0 +1,23 @@
+const sacolao = []
+
+const fruta1 = {
+    nome: "Maçã",
+    preco: 2.50,
+    disponibildade: true,
+}
+
+const fruta2 = {
+    nome: "Abacaxi",
+    preco: 9.00,
+    disponibildade: true,
+}
+
+const fruta3 = {
+    nome: "Tamarindo",
+    preco: 16.80,
+    disponibildade: false,
+}
+
+sacolao.push(fruta1, fruta2, fruta3)
+
+console.log(sacolao)
